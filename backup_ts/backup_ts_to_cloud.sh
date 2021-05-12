404b52ebfbf3c155a4182ed65dbcbcd290c669ce
Valentin Heidelberger <valh@mailbox.org>
2021-05-12 17:11:40 +0200

    add progress output

--- backup_ts/backup_ts_to_cloud.sh
+++ backup_ts/backup_ts_to_cloud.sh
@@ -11,16 +11,30 @@ TS_DATA_FOLDER="${3}"
 RCLONE_REMOTE="${4}"
 RCLONE_REMOTE_FOLDER="ts_backups"
 
+echo "Creating backup folder..."
 mkdir -p "${BACKUP_FOLDER}"
+echo ""
 
+echo "Dumping TS database..."
 docker exec "${MARIADB_CONTAINER_NAME}" sh -c 'exec mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > "${BACKUP_FOLDER}/ts_db_backup_${TODAY}.sql"
+echo ""
 
+echo "Copying TS data folder to backup folder..."
 rsync -a "${TS_DATA_FOLDER}" "${BACKUP_FOLDER}"
+echo ""
 
+echo "Zipping backup folder..."
 7z a "${BACKUP_ARCHIVE}" "${BACKUP_FOLDER}"
+echo ""
 
+echo "Encrypting backup archive..."
 python3 -c 'from plasm import encrypt; encrypt.encrypt_file("'"${BACKUP_ARCHIVE}"'", "'"${BACKUP_PUBLIC_KEY}"'", remove_input_file=True)'
+echo ""
 
+echo "Copying encrypted backup archive to cloud..."
 rclone copy "${BACKUP_ARCHIVE}.crypt" "${RCLONE_REMOTE}:${RCLONE_REMOTE_FOLDER}/"
+echo ""
 
+echo "Removing backup folder..."
 rm -rf "${BACKUP_FOLDER}"
+echo ""
