6564c24e423c750ac7d150935dc566d4fe997b16
Valentin Heidelberger <valh@mailbox.org>
2021-05-19 12:46:13 +0200

    update cping

--- cping/cping.sh
+++ cping/cping.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Usage:
-#conping <port> <address>
+#cping <port> <address>
 
 online=0
 port=$1
@@ -8,14 +8,12 @@ address=$2
 echo "$(date +"%Y-%m-%d %H:%M:%S") Started pinging"
 while [ $online -ne 1 ]
   do
+  answer=$(nmap "$address" -p "$port" | grep open)
 
-  answer=$(nmap $address -PN -p $port | grep open)
-
-  if [[ $answer = *"open"* ]]
+  if [[ $answer = "*open*" ]]
   then echo "$(date +"%Y-%m-%d %H:%M:%S") Port $port on $address is reachable now."
   online=1
   fi
 
   sleep 1
-
 done
