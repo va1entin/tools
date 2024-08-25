98045486d4758da4b95139e9c6ab0e243c56b6d4
Valentin Heidelberger <valh@mailbox.org>
2024-08-25 17:31:39 +0200

    export ts volume via podman, use tar xz instead of 7zip, remove sudo because job runs in user scope now

--- backup_ts/backup_ts_to_cloud.sh
+++ backup_ts/backup_ts_to_cloud.sh
@@ -4,12 +4,10 @@ TODAY="$(date +'%Y_%m_%d')"
 
 BACKUP_NAME="ts_backup_${TODAY}"
 BACKUP_FOLDER="/tmp/${BACKUP_NAME}"
-BACKUP_ARCHIVE="${BACKUP_FOLDER}.zip"
+BACKUP_ARCHIVE="${BACKUP_FOLDER}.tar.xz"
 BACKUP_PUBLIC_KEY="${1}"
 MARIADB_CONTAINER_NAME="${2}"
-TS_DATA_FOLDER="${3}"
-RCLONE_REMOTE="${4}"
-PODMAN_USER="${5}"
+RCLONE_REMOTE="${3}"
 RCLONE_REMOTE_FOLDER="ts_backups"
 ENCRYPTED_BACKUP_ARCHIVE="${BACKUP_ARCHIVE}.crypt"
 REMOTE_RETENTION="5d"
@@ -20,23 +18,15 @@ mkdir "${BACKUP_FOLDER}"
 echo ""
 
 echo "Dumping TS database..."
-sudo -u "${PODMAN_USER}" podman exec "${MARIADB_CONTAINER_NAME}" sh -c 'exec mariadb-dump teamspeak -uroot -p"$MARIADB_ROOT_PASSWORD"' > "${BACKUP_FOLDER}/ts_db_backup_${TODAY}.sql"
+podman exec "${MARIADB_CONTAINER_NAME}" sh -c 'exec mariadb-dump teamspeak -uroot -p"$MARIADB_ROOT_PASSWORD"' > "${BACKUP_FOLDER}/ts_db_backup_${TODAY}.sql"
 echo ""
 
-echo "Getting TS podman volume ID..."
-TS_PODMAN_VOLUME_ID=$(sudo -u "${PODMAN_USER}" podman inspect --format '{{json .HostConfig.Binds }}' teamspeak | grep -oP '[a-f0-9]+(?=:/var/ts3server)')
-echo "Found TS volume ID: ${TS_PODMAN_VOLUME_ID}"
-echo "Getting host mountpoint for TS podman volume..."
-TS_PODMAN_VOLUME_HOST_MOUNTPOINT=$(sudo -u "${PODMAN_USER}" podman volume inspect --format '{{json .Mountpoint }}' "${TS_PODMAN_VOLUME_ID}" | sed 's/"//g')
-echo "Found TS volume host mountpoint: ${TS_PODMAN_VOLUME_HOST_MOUNTPOINT}"
-echo ""
-
-echo "Copying data from TS podman volume to backup folder..."
-rsync -a "${TS_PODMAN_VOLUME_HOST_MOUNTPOINT}" "${BACKUP_FOLDER}"
+echo "Exporting TS podman volume..."
+podman volume export -o "${BACKUP_FOLDER}/ts_volume_export_${TODAY}.tar" teamspeak
 echo ""
 
 echo "Zipping backup folder..."
-7z a "${BACKUP_ARCHIVE}" "${BACKUP_FOLDER}"
+tar -cJf "${BACKUP_ARCHIVE}" -C "${BACKUP_FOLDER}" .
 echo ""
 
 echo "Encrypting backup archive..."
